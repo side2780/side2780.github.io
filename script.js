2f2cf81633ef28c2ee12b156dd5ed17d826873e7
side2780 <60001569+side2780@users.noreply.github.com>
2023-08-25 21:44:40 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -12,32 +12,35 @@ document.getElementById('calculateBtn').addEventListener('click', function () {
   const odiumCosts = odiumCost(odiumBefore, odiumAfter);
   const totalCosts = cerniumCosts + arcusCosts + odiumCosts;
 
-  document.getElementById('cernium result').textContent = `${cerniumCosts.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}楓幣`;
-  document.getElementById('arcus result').textContent = `${arcusCosts.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}楓幣`;
-  document.getElementById('odium result').textContent = `${odiumCosts.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}楓幣`;
-  document.getElementById('total result').textContent = `${totalCosts.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}楓幣`;
+  document.getElementById('cernium result').textContent = `${cerniumCosts.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}`;
+  document.getElementById('arcus result').textContent = `${arcusCosts.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}`;
+  document.getElementById('odium result').textContent = `${odiumCosts.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}`;
+  document.getElementById('total result').textContent = `${totalCosts.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}`;
 });
 
 function cerniumCost(before, after) {
   var cost = 0;
-  for (var i = before; i < after; i ++) {
+  if (before < 1 || after > 11) {return 0;}
+  for (var i = before; i < after; i++) {
     cost += (96900000 + 88500000 * i);
   }
-  return cost
+  return cost;
 }
 
 function arcusCost(before, after) {
   var cost = 0;
-  for (var i = before; i < after; i ++) {
+  if (before < 1 || after > 11) {return 0;}
+  for (var i = before; i < after; i++) {
     cost += (106600000 + 97300000 * i);
   }
-  return cost
+  return cost;
 }
 
 function odiumCost(before, after) {
   var cost = 0;
-  for (var i = before; i < after; i ++) {
+  if (before < 1 || after > 11) {return 0;}
+  for (var i = before; i < after; i++) {
     cost += (117400000 + 107100000 * i);
   }
-  return cost
+  return cost;
 }
